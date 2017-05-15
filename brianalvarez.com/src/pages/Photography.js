06bcb62d927e5ce58b3baa2f695cc715f4a35903
alvarezb <brianalvarez@Brian-Admin.local>
2017-05-15 12:18:19 -0700

    Moving to react-photo-gallery, and putting up test page

--- brianalvarez.com/src/pages/Photography.js
+++ brianalvarez.com/src/pages/Photography.js
@@ -1,89 +1,197 @@
-import React from 'react'
+import React from 'react';
+import Gallery from 'react-photo-gallery';
+import _ from 'lodash';
+import $ from 'jquery';
+import Measure from 'react-measure';
+import Lightbox from 'react-images';
 
-/*
-className About extends Component {
-  render() {
-    return (
-      <div>
-        <h2>About</h2>
+// eslint-disable-next-line
+const images = [
+{"name": "0058", "alt":"Tesla Coil", "wl": 1200, "hl": 800, "wm": 600, "hm": 400, "ws":300, "hs": 200},
+{"name": "8299", "alt":"Tesla Coil", "wl": 1200, "hl": 800, "wm": 600, "hm": 400, "ws":300, "hs": 200},
+{"name": "8484", "alt":"Tesla Coil", "wl": 1200, "hl": 800, "wm": 600, "hm": 400, "ws":300, "hs": 200},
+{"name": "8959", "alt":"Tesla Coil", "wl": 1200, "hl": 800, "wm": 600, "hm": 400, "ws":300, "hs": 200},
+{"name": "9106", "alt":"Tesla Coil", "wl": 1200, "hl": 800, "wm": 600, "hm": 400, "ws":300, "hs": 200},
+]
+
+class Photography extends React.Component{
+  constructor(){
+    super();
+    this.state = {photos:null, pageNum:1, totalPages:1, loadedAll: false, currentImage:0};
+    this.handleScroll = this.handleScroll.bind(this);
+    this.loadMorePhotos = this.loadMorePhotos.bind(this);
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  componentDidMount() {
+    this.loadMorePhotos();
+    this.loadMorePhotos = _.debounce(this.loadMorePhotos, 200);
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  handleScroll(){
+    let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+    if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+      this.loadMorePhotos();
+    }
+  }
+  loadMorePhotos(e){
+    if (e){
+      e.preventDefault();
+    }
+    if (this.state.pageNum > this.state.totalPages){
+      this.setState({loadedAll: true});
+      return;
+    }
+    $.ajax({
+      url: 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=7bfd5b3a1565cc08941231f01555d959&photoset_id=72157680618369974&user_id=143354752@N07&format=json&per_page=21&page='+this.state.pageNum+'&extras=url_m,url_c,url_l,url_h,url_o',
+      dataType: 'jsonp',
+      jsonpCallback: 'jsonFlickrApi',
+      cache: false,
+      success: function(data) {
+        console.log(data)
+        let photos = [];
+        data.photoset.photo.forEach(function(obj,i,array){
+          photos.push({
+            src: obj.url_m,
+            // eslint-disable-next-line
+            width: parseInt(obj.width_m),
+            // eslint-disable-next-line
+            height: parseInt(obj.height_m),
+            caption: obj.title,
+            alt: obj.title,
+            srcset:[
+            obj.url_m+' '+obj.width_m+'w',
+            obj.url_c+' '+obj.width_c+'w',
+            obj.url_h+' '+obj.width_h+'w'
+            ],
+            sizes:[
+            '(min-width: 480px) 33.3vw',
+            '(min-width: 1024px) 50vw',
+            '100vw'
+            ]  
+          });
+        })
+        this.setState({
+          photos: this.state.photos ? this.state.photos.concat(photos) : photos,
+          pageNum: this.state.pageNum + 1,
+          totalPages: data.photoset.pages
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }
+    });
+  }
+  /*
+    loadMorePhotos(e){
+    if (e){
+      e.preventDefault();
+    }
+    if (this.state.pageNum > this.state.totalPages){
+      this.setState({loadedAll: true});
+      return;
+    }
+        let photos = [];
+        images.forEach(function(obj,i,array){
+          let aspectRatio = parseFloat(obj.wl / obj.hl);
+          photos.push({
+            src: (aspectRatio >= 3) ? obj.url_c : obj.url_m,
+                    // eslint-disable-next-line
+                    width: parseInt(obj.ws),
+                    // eslint-disable-next-line
+                    height: parseInt(obj.hs),
+                    caption: obj.title,
+                    alt: obj.title,
+                    srcset:[ 
+                    'images/'+obj.name+'-lg.jpg '+obj.wl+'w',
+                    'images/'+obj.name+'-md.jpg '+obj.wm+'w',
+                    'images/'+obj.name+'-sm.jpg '+obj.ws+'w',
+                    ],
+                    sizes:[
+                    '(min-width: 480px) 50vw',
+                    '(min-width: 1024px) 33.3vw',
+                    '100vw'
+                    ]  
+                  });
+        })
+        this.setState({
+          photos: this.state.photos ? this.state.photos.concat(photos) : photos,
+          pageNum: this.state.pageNum + 1,
+          //totalPages: data.photoset.pages
+        });
+  }
+  */
+  openLightbox(index, event){
+    event.preventDefault();
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true
+    });
+  }     
+  closeLightbox(){
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious(){
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext(){
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  renderGallery(){
+    return(
+      <Measure whitelist={['width']}>
+      {
+        ({ width }) => {
+          var cols = 1;
+          if (width >= 480){
+            cols = 2;
+          }
+          if (width >= 1024){
+            cols = 3;
+          }
+          return <Gallery photos={this.state.photos} cols={cols} onClickPhoto={this.openLightbox} />
+        }
+      }
+      </Measure>
+      );
+  }
+  render(){
+  // no loading sign if its all loaded
+  if (this.state.photos){
+    return(
+      <div className="App">
+      {this.renderGallery()}
+      <Lightbox 
+      images={this.state.photos}
+      backdropClosesModal={true}
+      onClose={this.closeLightbox}
+      onClickPrev={this.gotoPrevious}
+      onClickNext={this.gotoNext}
+      currentImage={this.state.currentImage}
+      isOpen={this.state.lightboxIsOpen}
+      width={1600}
+      />
+      {!this.state.loadedAll && <div className="loading-msg" id="msg-loading-more">Loading</div>}
       </div>
-    );
+      );
+  }
+  else{
+    return(
+      <div className="App">
+      <div id="msg-app-loading" className="loading-msg">Loading</div>
+      </div>
+      );
   }
 }
-*/
+};
 
-
-const Photography = () => (
-  <div id="photography" role="tabpanel" className="tab-pane col-6 offset-lg-1">
-    <h3>Photography</h3>
-    <p>Photography is cool yo. Sharing photos is also cool. Occasionally I take photos that I think are cool enough to share</p>
-    <div>
-      <div id="myCarousel" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-          <li data-target="#myCarousel" data-slide-to="3"></li> 
-          <li data-target="#myCarousel" data-slide-to="4"></li>
-          <li data-target="#myCarousel" data-slide-to="5"></li>
-        </ol>
-        <div className="carousel-inner" role="listbox">
-          <div className="carousel-item active">
-            <img className="d-block img-fluid" src="images/0058-sm.jpg" alt="Tesla coil" srcSet="images/0058-sm.jpg 300w, images/0058-md.jpg 600w, images/0058-lg.jpg 1200w"/>
-            <div className="carousel-caption d-none d-md-block">
-              <h4>Tesla coil lightening</h4>
-              <p className=" d-none d-lg-block">Zapping musical lightening After Dark and the Exploratorium.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block img-fluid" src="images/8484-sm.jpg" alt="Breathing fire" srcSet="images/8484-sm.jpg 300w, images/8484-md.jpg 600w, images/8484-lg.jpg 1200w"/>
-            <div className="carousel-caption">
-              <h4>Releasing my inner dragon</h4>
-              <p className=" d-none d-lg-block">Lighting up a cold, dark night with my breath</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block img-fluid" src="images/8299-sm.jpg" alt="Spinning fire ax" srcSet="images/8299-sm.jpg 300w, images/8299-md.jpg 600w, images/8299-lg.jpg 1200w"/>
-            <div className="carousel-caption">
-              <h4>Spinning Fire</h4>
-              <p className=" d-none d-lg-block">Clark spinning the custom fire ax he made</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block img-fluid" src="images/8959-sm.jpg" alt="Pelicans against sunset" srcSet="images/8959-sm.jpg 300w, images/8959-md.jpg 600w, images/8959-lg.jpg 1200w"/>
-            <div className="carousel-caption">
-              <h4>Pelicans against a Marin sunset</h4>
-              <p className=" d-none d-lg-block">Enjoying the views at the north end of Point Reyes National Seashore, Tomales Point</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block img-fluid" src="images/9106-sm.jpg" alt="Elk Silhouettes" srcSet="images/9106-sm.jpg 300w, images/9106-md.jpg 600w, images/9106-lg.jpg 1200w"/>
-            <div className="carousel-caption">
-              <h4>Patrolling the Ridgeline</h4>
-              <p className=" d-none d-lg-block">Silhouetted elk watch me during a night hike at Point Reyes</p>
-            </div>
-          </div>
-        </div>
-        <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
-
-      <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-  </div>
-)
-
-export default Photography;
+export default Photography
